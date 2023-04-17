bb6c0dbf7096d5d74cc5928982221004c6318ba2
devpark435 <devpark435@gmail.com>
2023-04-17 13:29:42 +0900

    Create Project

---
+++ e_medicine/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  e_medicine
+//
+//  Created by 박현렬 on 2023/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ e_medicine/e_medicineApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_medicineApp.swift
+//  e_medicine
+//
+//  Created by 박현렬 on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct e_medicineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
